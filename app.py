3197e0f81b933a2d11b8f2f8a630390231d41cbe
Rubén Pérez Gracia <ruben.pergra05@gmail.com>
2025-05-23 04:48:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import paho.mqtt.client as mqtt
 import ssl
+import time
 
 app = Flask(__name__)
 
@@ -35,9 +36,17 @@ def enviar():
     mqtt_client = mqtt.Client(transport="websockets")
     mqtt_client.tls_set(cert_reqs=ssl.CERT_NONE)
     mqtt_client.tls_insecure_set(True)
+            # Define on_connect callback
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Conectado correctamente")
+            client.publish(MQTT_TOPIC, mensaje)
+        else:
+            print(f"Falló la conexión: {rc}")
+    mqtt_client.on_connect = on_connect
     mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
     mqtt_client.loop_start()
-    mqtt_client.publish(TOPIC_DATOS, "pizzas_hechas: 1")  # Por ejemplo
+    time.sleep(0.5)
     mqtt_client.loop_stop()
     return 'Pedido enviado'
 
